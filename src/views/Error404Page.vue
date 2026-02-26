d7ca4125e3fa07a8a7c02c48e948472c12bc7788
Pau Ojeda Gallego <pojeda@elpuig.xeill.net>
2026-02-26 16:38:07 +0100

    Commit

--- src/views/Error404Page.vue
+++ src/views/Error404Page.vue
@@ -9,7 +9,7 @@
   -->
   <main class="grid min-h-full place-items-center bg-[#222] px-6 py-24 sm:py-32 lg:px-8">
     <div class="text-center">
-      <p class="text-base font-semibold text-indigo-400">404</p>
+      <p class="text-3xl font-semibold text-indigo-400">404</p>
       <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-white sm:text-7xl">Página no encontrada</h1>
       <p class="mt-6 text-lg font-medium text-pretty text-gray-400 sm:text-xl/8">Lo sentimos, no pudimos encontrar la página que estabas buscando.</p>
       <div class="mt-10 flex items-center justify-center gap-x-6">
--- src/views/TabMessagesPage.vue
+++ src/views/TabMessagesPage.vue
@@ -7,15 +7,58 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <div class="my-10">
-        <div v-for="amigo in amigos" class="flex flex-col items-center mt-6 sm:mt-7 sm:items-start sm:ml-15">
-          <Amigo 
-            :name=amigo.name 
-            :lastMessage=amigo.lastMessage 
-            :img=amigo.img
-            :noLeido=amigo.noLeido>
-          </Amigo>
+
+      <div class="flex h-full">
+        <!-- 1 parte de la pagina, contactos -->
+
+        <div class="sm:w-3/7 lg:w-2/7 w-full sm:border-r sm:border-gray-700 justify-items-center sm:overflow-y-auto">
+          <div v-for="amigo in amigos" :key="amigo.id" class="flex flex-col items-center mt-6 sm:mt-7 sm:items-start sm:ml-15">
+            <Amigo
+              :name=amigo.name 
+              :lastMessage=amigo.lastMessage 
+              :img=amigo.img
+              :noLeido=amigo.noLeido
+              @click="seleccionarAmigo(amigo)">
+            </Amigo>
+          </div>
         </div>
+
+        <!-- 2 parte de la pagina, ordenador (chat) -->
+        <div class="hidden sm:w-2/3 lg:w-6/7 sm:flex flex-col">
+          
+          <div v-if="amigoSeleccionado" class="flex flex-col h-full">
+
+            <div class="p-4 border-b border-gray-700 flex flex-row gap-5">
+              <Image :src="amigoSeleccionado.img" imageClass="w-20 h-20 rounded-full ml-[3vw]" preview/>
+              <h2 class="text-lg font-bold m-auto truncate">
+                {{ amigoSeleccionado.name }}
+              </h2>            
+            </div>
+
+            <div class="flex-1 p-4 overflow-y-auto">
+              <div class="mb-2">
+                {{ amigoSeleccionado.lastMessage }}
+              </div>
+            </div>
+
+            <div class="p-4 rounded-xl border-t border-gray-700">
+              <input 
+                type="text"
+                class="w-[85%] p-2.5 rounded-xl bg-gray-800 text-white"
+                placeholder="Escribe un mensaje..."
+              />
+              <ion-icon aria-hidden="true" :icon="constructOutline" role="img" />
+            </div>
+
+          </div>
+
+          <!-- Si no hay chat seleccionado -->
+          <div v-else class="flex items-center justify-center h-full text-gray-500">
+            Selecciona una conversación
+          </div>
+
+        </div>
+
       </div>
     </ion-content>
   </ion-page>
@@ -23,11 +66,18 @@
 
 <script setup lang="ts">
 import Amigo from '@/components/Amigo.vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/vue';
 import { ref } from 'vue';
 import { dbamigos } from '@/components/datos';
+import Image from 'primevue/image';
 
 let amigos = ref(dbamigos);
+
+const amigoSeleccionado = ref();
+
+const seleccionarAmigo = (amigo:any) => {
+  amigoSeleccionado.value = amigo;
+}
 
 </script>
 
